9827814fe3a9cd1a7054c6dd118341eb2c97599c
Anandkumar.m <anandkumar.m@Marthanda.local>
2018-01-31 14:47:39 +0530

    test

---
+++ src/org/k9/demo/One.groovy
@@ -0,0 +1,17 @@
+package org.k9.demo 
+
+class One {
+  def config
+  def script
+
+  One(script, config) {
+    this.config = config
+    this.script = script
+  }
+
+  void tdemo() {
+    this.script.stage('Building Image') {
+       this.script.echo "stage 1" + this.config.version
+    }
+  }
+}
