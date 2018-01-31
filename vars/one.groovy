7777ba63c666cd14c41581cc8cb9ace1f5f97168
marthanda93 <marthanda93@users.noreply.github.com>
2018-01-31 14:33:27 +0530

    Update one.groovy

--- vars/one.groovy
+++ vars/one.groovy
@@ -7,7 +7,7 @@ def call(body) {
 
     node {
         stage('stage 1') {
-            echo "stage 1"
+            echo "stage 1 ${config.abc}"
         }
         stage('stage 2') {
             echo "statge 2"
