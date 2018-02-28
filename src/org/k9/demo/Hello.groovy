b69d7ba4bb7bbdbc879681686d7f06af8a1cfde2
srinivasdevops376 <35996741+srinivasdevops376@users.noreply.github.com>
2018-02-28 16:34:42 +0530

    Update Hello.groovy

--- src/org/k9/demo/Hello.groovy
+++ src/org/k9/demo/Hello.groovy
@@ -1,7 +1,18 @@
+
 package org.k9.demo 
 
-class Hello {
-  static def Hello(script, args) {
-     echo "Hello World"    
+class One {
+  def config
+  def script
+
+  One(script, config) {
+    this.config = config
+    this.script = script
+  }
+
+  void tdemo() {
+    this.script.stage('Building Image') {
+       this.script.echo "stage 1" + this.config.version
+    }
   }
 }
