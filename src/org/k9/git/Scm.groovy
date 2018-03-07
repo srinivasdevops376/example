aad957a083e4d7aff08dae3a716af9c0bb6232b0
srinivasdevops376 <35996741+srinivasdevops376@users.noreply.github.com>
2018-03-07 16:32:21 +0530

    Update Scm.groovy

--- src/org/k9/git/Scm.groovy
+++ src/org/k9/git/Scm.groovy
@@ -1,7 +1,32 @@
-package org.k9.git;
+package org.k9.git
+import org.k9.*
 
-def checkOutFrom(repo) {
-  git url: "git@github.com:jenkinsci/${repo}"
+class Git implements Serializable {
+  def config
+  def script
+
+  Git(script,config) {
+    this.config = config
+    this.script = script
+  }
+
+  void checkout(def url=false, def branch=false, cid=false) {
+    if (url != false) {
+       this.config.url = url
+       if (branch != false) {
+           this.config.branch = branch
+       }
+       if (cid != false && cid.length()>0) {
+           this.config.cid = cid
+       }
+    }
+    if (this.config.url) {
+       this.script.stage('Initialize') {
+         this.script.git(url: this.config.url, branch: this.config.branch.trim(), credentialsId: cid)
+       }
+    } else {
+        error "no URL found for git config --get remote.origin.url "
+    }
+  }
+
 }
-
-return this
